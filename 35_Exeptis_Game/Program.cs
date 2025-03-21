638e9c5a59740d5da7bea4162368b832564b5cb2
Mohammad Jabir <mohammadj94@gmail.com>
2025-03-21 01:32:53 -0600

    initial commit

---
+++ 35_Exeptis_Game/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _35_Exeptis_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
